ee56846bf3f5a6a07763b87ddbdd51cee7a25e78
Slepeez <barbalhoa78@gmail.com>
2026-08-27 15:46:07 -0300

    fix: just for remove the rest of files in the paste for github

--- tools/assembler.c
+++ tools/assembler.c
@@ -1,12 +1,8 @@
+#include "bin_header.h"
+#include "instruction.h"
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "bin_header.h"
-#include "instruction.h"
 int htnos_custom(uint16_t val) { return (val << 8) | (val >> 8); }
-uint16_t make_instruction{
-  
-}
-int main() {
-}
-
+uint16_t make_instruction {}
+int main() {}
